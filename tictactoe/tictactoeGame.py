c3fef669450e854f1d0896c288b1e2710440eb8c
keremoner <kerem.onr@gmail.com>
2020-10-23 15:10:40 +0300

    deneme

---
+++ tictactoe/tictactoeGame.py
@@ -0,0 +1,114 @@
+from __future__ import print_function
+import sys
+sys.path.append('..')
+from Game import Game
+from .tictactoeLogic import Board
+import numpy as np
+
+"""
+Game class implementation for the game of TicTacToe.
+Based on the OthelloGame then getGameEnded() was adapted to new rules.
+
+Author: Evgeny Tyurin, github.com/evg-tyurin
+Date: Jan 5, 2018.
+
+Based on the OthelloGame by Surag Nair.
+"""
+class tictactoeGame(Game):
+    def __init__(self):
+        pass
+        
+
+    def getInitBoard(self):
+        # return initial board (numpy board)
+        b = Board()
+        return np.array(b.pieces)
+
+    def getBoardSize(self):
+        # hole number
+        return (3,3)
+
+    def getActionSize(self):
+        # return number of actions
+        return 9
+
+    def getNextState(self, board, player, action):
+        # if player takes action on board, return next (board,player)
+        # action must be a valid move
+        
+        b = Board()
+        b.pieces = np.copy(board)
+        move = action
+        b.execute_move(move, player)
+        return b.pieces
+
+    def getValidMoves(self, board, player):
+        # return a fixed size binary vector
+        valids = [0]*self.getActionSize()
+        b = Board()
+        b.pieces = np.copy(board)
+        legalMoves =  b.get_legal_moves(player)
+        #print(legalMoves)
+        if len(legalMoves)==0:
+            valids[-1]=1
+            return np.array(valids)
+        for i in legalMoves:
+            valids[i] = 1
+        return np.array(valids)
+        
+    def getGameEnded(self, board, player):
+        # return 0 if not ended, 1 if player 1 won, -1 if player 1 lost
+        # player = 1
+        b = Board()
+        b.pieces = np.copy(board)
+        if b.is_win(player):
+            return 1
+        if b.is_win(-player):
+            return -1
+        if b.has_legal_moves():
+            return 0
+        # draw has a very little value 
+        return 1e-4
+              
+
+    def getCanonicalForm(self, board, player):
+        # return state if player==1, else return -state if player==-1
+
+        b = Board()
+        b.pieces = np.copy(board)
+        return player*b.pieces
+
+
+    def getSymmetries(self, board, pi):
+        # mirror, rotational
+        assert(len(pi) == self.n**2+1)  # 1 for pass
+        pi_board = np.reshape(pi[:-1], (self.n, self.n))
+        l = []
+
+        for i in range(1, 5):
+            for j in [True, False]:
+                newB = np.rot90(board, i)
+                newPi = np.rot90(pi_board, i)
+                if j:
+                    newB = np.fliplr(newB)
+                    newPi = np.fliplr(newPi)
+                l += [(newB, list(newPi.ravel()) + [pi[-1]])]
+        return l
+
+    def stringRepresentation(self, board):
+        # 8x8 numpy array (canonical board)
+        return np.array(board).tostring()
+
+    @staticmethod
+    def display(board):
+        newBoard = np.reshape(board, (3,3))
+        for row in newBoard:
+            for i in row:
+                if i == 0:
+                    print("_", end = " ")
+                elif i==1:
+                    print("X", end = " ")
+                elif i==-1:
+                    print("O", end = " ")
+            print("\n", end = "")
+            
---
+++ tictactoe/tictactoeLogic.py
@@ -0,0 +1,103 @@
+'''
+Board class for the game of TicTacToe.
+Default board size is 3x3.
+Board data:
+  1=white(O), -1=black(X), 0=empty
+  first dim is column , 2nd is row:
+     pieces[0][0] is the top left square,
+     pieces[2][0] is the bottom left square,
+Squares are stored and manipulated as (x,y) tuples.
+
+Author: Evgeny Tyurin, github.com/evg-tyurin
+Date: Jan 5, 2018.
+
+Based on the board for the game of Othello by Eric P. Nichols.
+
+'''
+
+# from bkcharts.attributes import color
+class Board():
+
+
+    def __init__(self):
+        "Set up initial board configuration."
+        self.n = 3
+        self.pieces = [0,0,0, 0,0,0, 0,0,0]
+        
+
+
+    # add [][] indexer syntax to the Board
+    def __getitem__(self, index): 
+        return self.pieces[index]
+
+    def get_legal_moves(self, color):
+        """Returns all the legal moves for the given color.
+        (1 for white, -1 for black)
+        @param color not used and came from previous version.        
+        """
+        moves = set()  # stores the legal moves.
+        
+        for i in range(9):
+            if self[i] == 0:
+                moves.add(i)
+        return list(moves)
+        
+          
+
+    def has_legal_moves(self, color):
+        for i in range(9):
+            if self[i] == 0:
+                return True
+        return False
+
+    def execute_move(self, move, color):
+        """Perform the given move on the board; 
+        color gives the color pf the piece to play (1=white,-1=black)
+        """
+        if color==1:
+            #replay stores if the player should act again
+            self[move]=1
+
+        else:
+            #replay stores if the player should act again
+            self[move]=-1
+                    
+     def is_win(self, color):
+        """Check whether the given player has collected a triplet in any direction; 
+        @param color (1=white,-1=black)
+        """
+        win = self.n
+        newBoard = np.reshape(np.array(self.pieces), (3,3))
+        
+        # check y-strips
+        for y in range(self.n):
+            count = 0
+            for x in range(self.n):
+                if newBoard[x][y]==color:
+                    count += 1
+            if count==win:
+                return True
+        # check x-strips
+        for x in range(self.n):
+            count = 0
+            for y in range(self.n):
+                if newBoard[x][y]==color:
+                    count += 1
+            if count==win:
+                return True
+        # check two diagonal strips
+        count = 0
+        for d in range(self.n):
+            if newBoard[d][d]==color:
+                count += 1
+        if count==win:
+            return True
+        count = 0
+        for d in range(self.n):
+            if newBoard[d][self.n-d-1]==color:
+                count += 1
+        if count==win:
+            return True
+        
+        return False
+
